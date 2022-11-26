5c3495c7b9dbbfdd66be4978c425a92275e3f2ce
Dima <dimabolandau0@gmail.com>
2022-11-26 21:44:13 +0200

    lectia 13

---
+++ lectia13/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const number = 21;
+
+switch(number) {
+    case 34:
+        console.log('ok');
+        break;
+    case 21:
+        console.log('bravo ma');
+        break;
+    default:
+        console.log('Alta data');
+}
