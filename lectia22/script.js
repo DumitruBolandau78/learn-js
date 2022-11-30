dc13a5360c7352359539f29e9071157546c7ec36
Dima <dimabolandau0@gmail.com>
2022-11-30 21:28:29 +0200

    dsa

--- lectia22/script.js
+++ lectia22/script.js
@@ -1,2 +1,18 @@
 'use strict'
 
+const obj = {
+    a: 10,
+    b: 3,
+    c : {
+        x: 12,
+        y: 15
+    }
+}
+
+const copyObj = JSON.parse(JSON.stringify(obj));
+
+copyObj.a = 21;
+copyObj.c.x = 51;
+
+console.log(obj);
+console.log(copyObj);
